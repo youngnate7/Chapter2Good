5a53a0041834e78d8c64139646a326db201af69d
Nathan <nathanyoung@billericak12.com>
2019-10-11 11:28:45 -0400

    yup

--- src/com/company/Fraction.java
+++ src/com/company/Fraction.java
@@ -32,31 +32,57 @@ public class Fraction {
         return result;
     }
 
-    // TODO  subtract()
+    public Fraction subtract(Fraction other){
+        int n = this.numerator*other.denominator - this.denominator*other.numerator;
+        int d = this.denominator * other.getDenom();
 
-    // TODO multiply()
+        Fraction result = new Fraction(n, d);
+        return result;
+    }
 
-    // TODO toString()
+    public Fraction multiply(Fraction other){
+        int n = this.numerator*other.numerator;
+        int d = this.denominator*other.getDenom();
+
+        Fraction result = new Fraction(n, d);
+        return result;
+    }
+
+    public String toString(){
+        String s = numerator + "/" + denominator;
+        return s;
+    }
 
 
     public static void main(String[] args) {
         Fraction f1 = new Fraction(3,4);	// create an instance of fraction
         Fraction f2 = new Fraction(4,5);
+        Fraction f6 = new Fraction(6, 9);
+        Fraction f7 = new Fraction(9, 6);
 
         Fraction f3 = f1.add(f2);			// add 2 fractions
         System.out.println("3/4 + 4/5 = " + f3);	// print the answer
 
-		/*
+
 		Fraction f4 = f1.subtract(f2);
 		System.out.println(f1 + " - " + f2 + " = " + f4);
 
-		Add code to call multiply f1 and f2 here
+		Fraction f5 = f1.multiply(f2);
+		System.out.println(f1 + " * " + f2 + " = " + f5);
 
-		create 2 new fractions and add them here.
+		Fraction f8 = f6.add(f7);
+		System.out.println(f6 + " + " + f7 + " = " + f8);
 
-		*/
+
 
 
     }
 
 }
+//Output:
+// 3/4 + 4/5 = 31/20
+//3/4 - 4/5 = -1/20
+//3/4 * 4/5 = 12/20
+//6/9 + 9/6 = 117/54
+//
+//Process finished with exit code 0
