b860bfd2a4e6014034340d41408e767e37457d27
nathan <nathanyoung@billericak12.com>
2019-10-18 12:36:56 -0400

    aaaaaaaaaaaa

--- src/com/company/Frog.java
+++ src/com/company/Frog.java
@@ -45,7 +45,8 @@ public class Frog {
     // the frog will move both up and right.  Returns no value.
 
     public void visit(Frog host){
-        
+        this.xPos = host.xPos;
+        this.yPos = host.yPos;
     }
 
     public void eatFly()
@@ -60,7 +61,6 @@ public class Frog {
 
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         Frog f1 = new Frog("Freddy",3,4);	// create an instance of Frog
         f1.eatFly();						// call Frog methods
         f1.moveNorth();
@@ -68,24 +68,32 @@ public class Frog {
 
         Frog f2 = new Frog("Francine", -2,0);
         f2.eatFly();
-        // TODO uncomment these to test new code.
-        //f2.jump(2);
-        //f1.visit(f2);
+        f2.jump(2);
+        f1.visit(f2);
+
+        Frog f3 = new Frog("Frank", 0, 0);
+        f3.jump(17);
+        f3.jump(69);
+        f3.visit(f1);
+
         System.out.println(f1);
         System.out.println(f2);
+        System.out.println(f3);
 
-        // make a new frog named Frank.  have him jump twice and then go visit Freddy.
+
 
     }
 
 }
 
-/*  Output
-Freddy
-Yum
-Hi I am Freddy the frog and I live at 3 5
-Francine
-Yum
-Hi I am Freddy the frog and I live at 0 2
-Hi I am Francine the frog and I live at 0 2
-*/
+// Freddy
+//Yum
+//Hi I am Freddy the frog and I live at 3 5
+//Francine
+//Yum
+//Frank
+//Hi I am Freddy the frog and I live at 0 2
+//Hi I am Francine the frog and I live at 0 2
+//Hi I am Frank the frog and I live at 0 2
+//
+//Process finished with exit code 0
